30b880653b7eb6c5178966cf0ad6ad8c409bfb20
Tobias Ephron <tobiasephron@gmail.com>
2021-11-11 22:06:18 -0500

    GUI Done

--- GUI.py
+++ GUI.py
@@ -51,23 +51,6 @@ class Grid:
                 self.update_model()
                 return False
 
-    def update_model(self):
-        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
-    
-    def place(self, val):
-        row, col = self.selected
-        if self.cubes[row][col].value == 0:
-            self.cubes[row][col].set(val)
-            self.update_model()
-
-            if valid(self.model, val, (row,col)) and self.solve():
-                return True
-            else:
-                self.cubes[row][col].set(0)
-                self.cubes[row][col].set_temp(0)
-                self.update_model()
-                return False
-
     # Function for sketching in a given value to a selected spot (cube)
     def sketch(self, val):
         row, col = self.selected
